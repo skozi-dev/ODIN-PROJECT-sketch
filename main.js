ac9f119dce8cadff19bf7daa590b818fe730a553
skozi-dev <n10895647@qut.edu.au>
2024-08-12 16:53:10 +1000

    Sketchpad drawing implemented using mouseover

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /* Create variables for grid */
-const gridSize = 16;
-const cellSize = 15;
+const gridSize = 30;
+const cellSize = 20;
 
 /*  Get the Sketchpad element */
 const sketchPad = document.querySelector('#sketchPad');
@@ -8,7 +8,7 @@ const sketchPad = document.querySelector('#sketchPad');
 /* Create the grid */
 const gridContainer = document.createElement('div');
 gridContainer.className = 'gridContainer';
-gridContainer.style.width = `${gridSize * cellSize}px`; // dynamic resizing of grid
+gridContainer.style.width = `${gridSize * cellSize + gridSize * 2}px`; // dynamic resizing of grid plus 2px per pixel outline
 sketchPad.appendChild(gridContainer);
 
 /* Create the cells of the grid */
@@ -17,8 +17,18 @@ function createSketchpad(size) {
 	for (let i = 1; i <= gridSquared; i++) {
 		const pixel = document.createElement('div');
 		pixel.className = 'pixel';
+		pixel.style.cssText = `height: ${cellSize}px; 
+            width: ${cellSize}px; 
+            border: 1px solid lightblue;
+            background-color: #f4f0e8;`;
 
-		gridContainer.appendChild(pixel);
+		gridContainer.append(pixel);
+
+		/* Create a hover effect for cells */
+		const hoverEffect = document.querySelectorAll('.pixel');
+		pixel.addEventListener('mouseover', () => {
+			pixel.style.background = 'red';
+		});
 	}
 }
 
