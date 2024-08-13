64ccb79c7783582271dc0d48114bfd74fda3a4bd
skozi-dev <n10895647@qut.edu.au>
2024-08-13 23:33:54 +1000

    created hover effect for each pixel to be colored red when moused over

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
 const canvas = document.querySelector('.canvas');
-const pixel = document.querySelectorAll('.pixel');
-
-// pixel.addEventListener('mouseover', () => {
-//     pixel.style.background = 'red';
-// });
-
 const numPixels = 16;
 
 /* Create a dynamic grid */
@@ -12,13 +6,19 @@ function createGrid() {
     for (let i = 0; i < numPixels; i++) {
         const pixelRow = document.createElement('div');
         pixelRow.classList.add('pixelRow', 'pixel');
-        canvas.appendChild(pixelRow);
+        canvas.append(pixelRow);
 
         for (let k = 0; k < numPixels; k++) {
             const pixelCol = document.createElement('div');
             pixelCol.classList.add('pixelCol', 'pixel');
-            pixelRow.appendChild(pixelCol);
+            pixelRow.append(pixelCol);
+            pixelCol.addEventListener('mouseover', function () {
+                pixelCol.style.background = 'red';
+            });
         }
+        pixelRow.addEventListener('mouseover', function () {
+            pixelRow.style.background = 'red';
+        });
     }
 }
 
