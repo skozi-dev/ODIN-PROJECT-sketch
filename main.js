12bbfa7d185c33029c31ded2996dd5c93162b2cd
skozi-dev <n10895647@qut.edu.au>
2024-08-13 15:09:27 +1000

    16 x 16 grid created and can be dynamically changed based off numPixels

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-const gridContainer = document.querySelector('.gridContainer');
+const canvas = document.querySelector('.canvas');
 
-function createGrid() {}
+const numPixels = 16;
+
+function createGrid() {
+    for (let i = 0; i < numPixels; i++) {
+        const pixelRow = document.createElement('div');
+        pixelRow.classList.add('pixelRow');
+        canvas.appendChild(pixelRow);
+
+        for (let k = 0; k < numPixels; k++) {
+            const pixelCol = document.createElement('div');
+            pixelCol.classList.add('pixelCol');
+            pixelRow.appendChild(pixelCol);
+        }
+    }
+}
+
+createGrid();
