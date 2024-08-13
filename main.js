371a12beab1e7728e60cddd8ca29430d7b46185d
skozi-dev <n10895647@qut.edu.au>
2024-08-13 15:29:55 +1000

    adding hovereffect

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 const canvas = document.querySelector('.canvas');
+const pixel = document.querySelectorAll('.pixel');
+
+// pixel.addEventListener('mouseover', () => {
+//     pixel.style.background = 'red';
+// });
 
 const numPixels = 16;
 
+/* Create a dynamic grid */
 function createGrid() {
     for (let i = 0; i < numPixels; i++) {
         const pixelRow = document.createElement('div');
-        pixelRow.classList.add('pixelRow');
+        pixelRow.classList.add('pixelRow', 'pixel');
         canvas.appendChild(pixelRow);
 
         for (let k = 0; k < numPixels; k++) {
             const pixelCol = document.createElement('div');
-            pixelCol.classList.add('pixelCol');
+            pixelCol.classList.add('pixelCol', 'pixel');
             pixelRow.appendChild(pixelCol);
         }
     }
